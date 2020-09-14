de0b3bc4c515f97ffcecc0ee9658e0fa7da67e0a
y2 <y2zaijian@126.com>
2020-09-14 23:53:50 +0800

    searcher

--- searcher/search.h
+++ searcher/search.h
@@ -7,10 +7,13 @@
 *
 ================================================================*/
 #pragma once
+#include "cppjieba/jieba.hpp"
 #include <iostream>
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <stdint.h>
+
 using std::string;
 using std::cout;
 using std::endl;
@@ -38,6 +41,7 @@ struct Weight{
     string word;
 };
 
+// 这个东西叫做“倒排拉链”
 typedef vector<Weight> InvertedList;
 
 // Index类用于整个索引结构, 并且提供一些供外部调用的API
@@ -50,6 +54,7 @@ class Index{
         unordered_map<string, InvertedList> inverted_index;
 
     public:
+        Index();
         // 提供一些对外调用的函数
         // 1. 查正排的函数, 返回指针就可以使用 NULL 表示无效结果的情况(引用没有表示无效的情况)
         const DocInfo* GetDocInfo(int64_t doc_id);
@@ -57,9 +62,15 @@ class Index{
         const InvertedList* GetInvertedList(const string &key);
         // 3. 构建索引
         bool Build(const string &input_path);
+
+    private:
+        DocInfo* BuildForward(const string& line);
+        void BuildInverted(const DocInfo& doc_info);
+        void CutWord(const string& input, vector<string>* output);
+        cppjieba::Jieba jieba;
 };
 
-// 接下来是模块内容
-}
+// 接下来是搜索模块的内容
+}// namespace searcher
 
 
